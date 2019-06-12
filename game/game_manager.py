4c337de3a2059822aa88db110f305ff50505f036
holyshining <holyshine@yahoo.com>
2019-06-12 08:40:14 +0300

    Create game model

---
+++ game/game_manager.py
@@ -0,0 +1,5 @@
+class GameManager:
+    def __init__(self, number_of_players, sequence, deck):
+        self.number_of_players = number_of_players
+        self.sequence = sequence
+        self.deck = deck
--- game/models.py
+++ game/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class GameHistory(models.Model):
+    """Model for storing results of every game at database"""
+
+    class Meta:
+        db_table = 'game_history'
+        ordering = ['id']
+
+    players = models.IntegerField(null=True)
+    squares = models.IntegerField(null=True)
+    cards = models.IntegerField(null=True)
+    sequence = models.CharField(max_length=79, null=True)
+    deck = models.TextField(max_length=600, null=True)
+    result = models.CharField(max_length=60, null=True)
